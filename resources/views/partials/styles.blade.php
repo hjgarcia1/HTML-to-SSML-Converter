1ccc82093fae6726c823fba63130f013b2451bac
Henry <Garcia>
2020-04-15 20:30:47 -0400

    added new fonts

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,10 +1,16 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 <style>
     html, body {
         height: 100%;
+        font-family: 'Open Sans', sans-serif;
     }
+
+    h1, h2, h3, h4, h5, h6, .nav-link {
+        font-family: 'Roboto', sans-serif;
+    }
+
     .grid {
         height: 100%;
         display: grid;
@@ -16,4 +22,5 @@
         margin-bottom: 15px;
         font-weight: bold;
     }
+
 </style>
